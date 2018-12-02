4886399f948e62d6f5f7abecba79b4e2f7dbb692
shortaflip <kevin.enario.354@my.csun.edu>
2018-12-01 21:57:55 -0800

    added registration page.

--- pages/home/register.vue
+++ pages/home/register.vue
@@ -7,10 +7,13 @@
     <v-text-field label="Date Of Birth" v-model="dateOfBirth"></v-text-field>
     <v-text-field label="E-mail Address" v-model="email"></v-text-field>
     <v-text-field label="Password" v-model="password"></v-text-field>
+    <v-btn @click.native="formValidation">Submit</v-btn>
   </v-container>
 </template>
 
 <script>
+  import moment from 'moment'
+
   export default {
     name: 'Register',
     data: () => ({
@@ -19,7 +22,36 @@
       lastName: '',
       dateOfBirth: '',
       email: '',
-      password: ''
-    })
+      password: '',
+      formattedDate: '',
+      registrationResponse: ''
+    }),
+    methods: {
+      async submitRegistration() {
+        await this.$axios ({
+          method: 'post',
+          url: 'http://localhost:8080/account/register',
+          headers: {'Content-Type': 'application/json'},
+          data: {
+            username: this.username,
+            firstname: this.firstName,
+            lastname: this.lastName,
+            dateofbirth: this.formattedDate,
+            email: this.email,
+            password: this.password            
+          } 
+        }).then(res => {
+          this.registrationResponse = res.data;
+          console.log(this.registrationResponse)
+        }).catch(err => {
+          console.log(err);
+        })
+      },
+      formValidation() {
+        this.formattedDate = moment(this.dateOfBirth).format('YYYY-MM-DD')        
+
+        this.submitRegistration()
+      }
+    }
   };
 </script>
