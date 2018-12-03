cfa391748707ea33ae74a33be55eac7733183a4e
shortaflip <kevin.enario.354@my.csun.edu>
2018-12-03 12:27:09 -0800

    added firebase initialization file and configurations.

---
+++ services/fireinit.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/database'
+import env from '@/middleware/env'
+
+var config = {
+    apiKey: env.apiKey,
+    authDomain: env.authDomain,
+    databaseURL: env.databaseURL,
+    projectId: env.projectId,
+    storageBucket: env.storageBucket,
+    messagingSenderId: env.messagingSenderId 
+}
+
+!firebase.apps.length ? firebase.initializeApp(config) : ''
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
+export const auth = firebase.auth()
+export const DB = firebase.database()
+export const StoreDB = firebase.firestore()
+export default firebase
