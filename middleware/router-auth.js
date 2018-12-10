b69f370e6e6c294a649915c10ac60a2c0ecf6e5a
shortaflip <kevin.enario.354@my.csun.edu>
2018-12-10 12:55:45 -0800

    fixed route authentication guard, wasn't working.

--- middleware/router-auth.js
+++ middleware/router-auth.js
@@ -3,8 +3,8 @@ export default function({ store, redirect, route }) {
   store.state.user == null && isAuthRoute(route) ? redirect('/home/login') : console.log(route.path)
 }
 
-function isAuthRoute(route) {
-  if (route.matched.some(record => record.path === '/u/')) {
+function isAuthRoute(route) {  
+  if (route.matched.some(record => record.path.match('/u/'))) {
     return true
   }
 }
